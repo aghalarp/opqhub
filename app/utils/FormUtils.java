72a88d50d58f3262e2f42de512d80cdd23f96b08
anthonyjchriste <anthony.christe@gmail.com>
2013-07-26 17:25:59 -1000

    Updated to include wireless carrier information.

--- app/utils/FormUtils.java
+++ app/utils/FormUtils.java
@@ -53,4 +53,25 @@ public class FormUtils {
     "Wisconsin",
     "Wyoming"
   };
+  // See: https://en.wikipedia.org/wiki/List_of_SMS_gateways
+  public static final String[][] smsCarriers = {
+    {"Alltel",        "#@sms.alltelwireless.com"},
+    {"AT&T",          "#@txt.att.net"},
+    {"Cricket",       "#@sms.myscricket.com"},
+    {"Sprint",        "#@messaging.sprintpcs.com"},
+    {"Straight Talk", "#@vtext.com"},
+    {"T-Mobile",      "#@tmomail.net"},
+    {"TracFone",      "#@mmst5.tracfone.com"},
+    {"Verizon",       "#@vtext.com"},
+    {"Virgin Mobile", "#@vmobl.com"}
+  };
+
+  public static String[] getSmsCarriers() {
+    String[] carriers = new String[smsCarriers.length];
+    for(int i = 0; i < carriers.length; i++) {
+      carriers[i] = smsCarriers[i][0];
+    }
+    return carriers;
+  }
+
 }
