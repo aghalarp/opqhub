e7af10400f91777ad5771839482d5c50b4f0e735
anthonyjchriste <anthony.christe@gmail.com>
2013-07-26 17:24:25 -1000

    Changed to work with new views/admin package.

--- app/controllers/Administration.java
+++ app/controllers/Administration.java
@@ -2,22 +2,26 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.admin.adminalert;
+import views.html.admin.admincdsi;
+import views.html.admin.admindevice;
+import views.html.admin.adminuser;
 
 public class Administration extends Controller {
 
   public static Result user() {
-    return ok(views.html.adminuser.render());
+    return ok(adminuser.render());
   }
 
   public static Result device() {
-    return ok(views.html.admindevice.render());
+    return ok(admindevice.render());
   }
 
   public static Result alert() {
-    return ok(views.html.adminalert.render());
+    return ok(adminalert.render());
   }
 
   public static Result cdsi() {
-    return ok(views.html.admincdsi.render());
+    return ok(admincdsi.render());
   }
 }
