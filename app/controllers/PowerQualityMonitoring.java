8d9db0b3de5ada68184101a99bcf00fab8bb4547
anthonyjchriste <anthony.christe@gmail.com>
2013-07-29 14:07:07 -1000

    Modified to work with new models.

--- app/controllers/PowerQualityMonitoring.java
+++ app/controllers/PowerQualityMonitoring.java
@@ -12,23 +12,8 @@ import java.util.List;
 public class PowerQualityMonitoring extends Controller {
   public static Result publicMonitor() {
     List<models.Alert> alerts = new LinkedList<>();
-    List<models.OpqDevice> devices = new LinkedList<OpqDevice>();
+    List<models.OpqDevice> devices = OpqDevice.find().where().eq("participatingInCdsi", true).findList();
 
-    // For mockup purposes, lets create some fake devices, and fake alerts.
-    devices.add(new OpqDevice(0L, 21.3069, -157.8583)); // Honolulu
-    devices.add(new OpqDevice(1L, 21.4022, -157.7394)); // Kailua
-    devices.add(new OpqDevice(2L, 21.3147, -157.8081)); // Manoa
-    devices.add(new OpqDevice(3L, 21.4447, -158.1900)); // Waianae
-    devices.add(new OpqDevice(4L, 21.6536, -157.9272)); // Laie
-    devices.add(new OpqDevice(5L, 21.4181, -157.8036)); // Kaneohe
-    devices.add(new OpqDevice(6L, 21.3147, -157.8081)); // Manoa
-    devices.add(new OpqDevice(7L, 21.3619, -157.9536)); // Pearl Harbor
-    devices.add(new OpqDevice(8L, 21.2695, -157.8196)); // Waikiki
-    devices.add(new OpqDevice(9L, 21.4511, -158.0156)); // Mililani
-
-    alerts.add(new Alert(Alert.AlertType.FREQUENCY, new Date(), 1000L, 57.8, "Low Frequency (-1.2 Hz)", 21.3069, -157.8583)); // Honolulu
-    alerts.add(new Alert(Alert.AlertType.VOLTAGE, new Date(), 2500L, 122.0, "High Voltage (+2 V)", 21.4022, -157.7394)); // Kailua
-    alerts.add(new Alert(Alert.AlertType.VOLTAGE, new Date(), 500L, 118.3, "Low Voltage (-1.7 V)", 21.3147, -157.8081)); // Manoa
 
     return ok(views.html.publicpowerqualitymonitoring.render(alerts, devices, true));
   }
