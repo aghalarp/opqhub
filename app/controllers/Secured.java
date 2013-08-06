7c5733fe14182e0e19d3d73813f491fe5dc6c28f
anthonyjchriste <anthony.christe@gmail.com>
2013-08-05 16:38:01 -1000

    Created authorization class

---
+++ app/controllers/Secured.java
@@ -0,0 +1,8 @@
+package controllers;
+
+/**
+ * Created with IntelliJ IDEA. User: anthony Date: 8/2/13 Time: 2:37 PM To change this template use File | Settings |
+ * File Templates.
+ */
+public class Secured {
+}
