27cdc5469748e150af056f88ce7701fd2d8004cb
anthonyjchriste <anthony.christe@gmail.com>
2014-02-10 10:11:02 -1000

    Menus are now in a correct working state, but still using the hack-ish method.

--- public/javascripts/opq-ao.js
+++ public/javascripts/opq-ao.js
@@ -12,3 +12,15 @@ function addDynamicJs (location) {
     jsRef.setAttribute("type", "text/javascript");
     document.getElementsByTagName("head")[0].appendChild(jsRef);
 }
+
+// It would be nice to eventually use the following code, but having multiple levels of menus on some pages
+// makes this difficult.
+/*
+function makeActiveClass() {
+  $('ul.nav a').filter(function() {
+    return this.href == document.location;
+  }).parent().addClass('active');
+}
+
+$(document).ready(makeActiveClass);
+*/
