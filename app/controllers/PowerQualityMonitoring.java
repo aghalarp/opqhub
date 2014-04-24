a105383432f33d4d083e5680cb67a4c5d5395a4c
anthonyjchriste <anthony.christe@gmail.com>
2014-04-23 14:47:08 -1000

    Almost working first iteration of public grid map. Squares are colored and updated, but a cyclic dependency in the grid-map library is causing a bug. #64

--- app/controllers/PowerQualityMonitoring.java
+++ app/controllers/PowerQualityMonitoring.java
@@ -19,13 +19,26 @@
 
 package controllers;
 
-import models.Event;
+import com.fasterxml.jackson.databind.JsonNode;
 
-import models.OpqDevice;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import models.Alert;
+import play.libs.Json;
+import play.mvc.BodyParser;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import models.Event;
+import models.OpqDevice;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Contains methods for modifying views and models for both private and public power quality monitoring.
@@ -42,6 +55,35 @@ public class PowerQualityMonitoring extends Controller {
     List<models.OpqDevice> devices = OpqDevice.find().where().eq("sharingData", true).findList();
     boolean loggedOut = !(session().containsKey("email"));
     return ok(views.html.publicmonitoring.publicpowerqualitymonitoring.render(events, devices, loggedOut));
+  }
+
+  @BodyParser.Of(BodyParser.Json.class)
+  public static Result alertsFromIds() {
+    JsonNode json = request().body().asJson();
+    Iterator<JsonNode> it = json.findValue("visibleIds").elements();
+    List<OpqDevice> devices = new LinkedList<>();
+    Set<String> squaresWithEvents = new HashSet<>();
+    int idLength = 0;
+
+    // Find devices
+    while(it.hasNext()) {
+      String partialId = it.next().asText();
+      idLength = partialId.length();
+      devices.addAll(OpqDevice.find().where().startsWith("gridId", partialId).findList());
+    }
+
+    //Find events associated with devices
+    for(OpqDevice device : devices) {
+      if(device.getEvents().size() > 0) {
+        squaresWithEvents.add(device.getGridId().substring(0, idLength));
+      }
+    }
+
+    // Respond with list of affected grid-squares
+    ObjectNode result = Json.newObject();
+    result.put("squaresWithEvents", Json.toJson(squaresWithEvents));
+
+    return ok(result);
   }
 
 }
