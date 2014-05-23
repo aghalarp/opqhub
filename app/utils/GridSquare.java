4aef4c047ac0f49b7c3ecdaff999f56a6ddcd39b
anthonyjchriste <anthony.christe@gmail.com>
2014-05-22 14:38:54 -1000

    Working itic plot in public map. Displays events from currently visible map area on plot. #70

--- app/utils/GridSquare.java
+++ app/utils/GridSquare.java
@@ -1,12 +1,35 @@
 package utils;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Utility class used for mapping a gridId -> set a metrics about that grid square.
  */
 public class GridSquare {
+  public class IticPoint {
+    public final double voltageValue;
+    public final long duration;
+
+    public IticPoint(final long duration, final double voltageValue) {
+      this.voltageValue = voltageValue;
+      this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+      return "[" + this.duration + "," + this.voltageValue + "]";
+    }
+  }
+
   public String gridId;
   public int numDevices = 0;
   public int numAffectedDevices = 0;
   public int numFrequencyEvents = 0;
   public int numVoltageEvents = 0;
+  public List<IticPoint> iticPoints = new LinkedList<>();
+
+  public void addIticPoint(long duration, double voltage) {
+    iticPoints.add(new IticPoint(duration, voltage));
+  }
 }
